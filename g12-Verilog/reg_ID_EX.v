1e4c29b8b894f74573f8be32b0f6de3e1f5265aa
jojimaaa <enzo.jojima@usp.br>
2025-12-12 15:30:37 -0300

    refact: changed implementation to Haris' -  Digital Design and Computer Architecture, RISC-V Edition.

--- g12-Verilog/reg_ID_EX.v
+++ g12-Verilog/reg_ID_EX.v
@@ -1,125 +1,121 @@
 module reg_ID_EX(
-    input         clock,
-    input         reset,
-    input         enable,
+    input        clock,
+    input        reset,
+    input        enable,
 
-    input  [31:0] PC_ID,
-    input  [31:0] RD1_ID,
-    input  [31:0] RD2_ID,
-    input  [31:0] ImmExt_ID,
-    input  [4:0]  RS1_ID, //a1 do regfile = instr[19:15]
-    input  [4:0]  RS2_ID, //a2 do regfile = instr[24:20]
-    input  [4:0]  RD_ID, //a3 do regfile = instr[11:7]
-    input  [2:0]  funct3_ID, //instr[14:12],
-    input         funct7b5_ID, //instr[30],
-    //sinais de controle
-    //EX
-    input         ALUSrc_ID,
-    input  [1:0]  ALUOp_ID,
-    //MEM
-    input         Branch_ID,
-    //WB
-    input         RegWrite_ID,
-    input         MemToReg_ID,
-    
-    output [31:0] PC_EX,
-    output [31:0] RD1_EX,
-    output [31:0] RD2_EX,
-    output [31:0] ImmExt_EX,
-    output [4:0]  RS1_EX, //a1 do regfile = instr[19:15]
-    output [4:0]  RS2_EX, //a2 do regfile = instr[24:20]
-    output [4:0]  RD_EX, //a3 do regfile = instr[11:7]
-    output [2:0]  funct3_EX, //instr[14:12],
-    output        funct7b5_EX, //instr[30],
-    //sinais de controle
-    //EX
-    output        ALUSrc_EX,
-    output [1:0]  ALUOp_EX,
-    //MEM
-    output        Branch_EX,
-    //WB
-    output        RegWrite_EX,
-    output        MemToReg_EX
+    input        RegWriteD,
+    input [1:0]  ResultSrcD,
+    input        MemWriteD,
+    input        JumpD,
+    input        BranchD,
+    input [2:0]  ALUControlD,
+    input        ALUSrcD,
+    input [1:0]  ImmSrcD,
+
+    input [31:0] RD1D,
+    input [31:0] RD2D,
+    input [31:0] PCD,
+    input [31:0] RdD,
+    input [31:0] ImmExtD,
+    input [31:0] PCPlus4D,
+
+
+    output        RegWriteE,
+    output [1:0]  ResultSrcE,
+    output        MemWriteE,
+    output        JumpE,
+    output        BranchE,
+    output [2:0]  ALUControlE,
+    output        ALUSrcE,
+    output [1:0]  ImmSrcE,
+    output [31:0] RD1E,
+    output [31:0] RD2E,
+    output [31:0] PCE,
+    output [31:0] RdE,
+    output [31:0] ImmExtE,
+    output [31:0] PCPlus4E,
 );
 
-    reg [31:0] PC;
+    reg        RegWrite;
+    reg [1:0]  ResultSrc;
+    reg        MemWrite;
+    reg        Jump;
+    reg        Branch;
+    reg [2:0]  ALUControl;
+    reg        ALUSrc;
+    reg [1:0]  ImmSrc;
     reg [31:0] RD1;
     reg [31:0] RD2;
+    reg [31:0] PC;
+    reg [31:0] Rd;
     reg [31:0] ImmExt;
-    reg [4:0]  RS1;
-    reg [4:0]  RS2;
-    reg [4:0]  RD;
-    reg [2:0]  funct3;
-    reg        funct7b5;
-    reg        ALUSrc;
-    reg [1:0]  ALUOp;
-    reg        Branch;
-    reg        RegWrite;
-    reg        MemToReg;
+    reg [31:0] PCPlus4;
 
     initial begin
-        PC <= 0;
+        RegWrite <= 0;
+        ResultSrc <= 0;
+        MemWrite <= 0;
+        Jump <= 0;
+        Branch <= 0;
+        ALUControl <= 0;
+        ALUSrc <= 0;
+        ImmSrc <= 0;
         RD1 <= 0;
         RD2 <= 0;
+        PC <= 0;
+        Rd <= 0;
         ImmExt <= 0;
-        RS1 <= 0;
-        RS2 <= 0;
-        RD <= 0;
-        funct3 <= 0;
-        funct7b5 <= 0;
-        ALUSrc <= 0;
-        ALUOp <= 0;
-        Branch <= 0;
-        RegWrite <= 0;
-        MemToReg <= 0;
+        PCPlus4 <= 0;
     end
 
     always @(posedge clock or posedge reset) begin
-        if (reset)
-            PC <= 0;
+        if (reset) begin
+            RegWrite <= 0;
+            ResultSrc <= 0;
+            MemWrite <= 0;
+            Jump <= 0;
+            Branch <= 0;
+            ALUControl <= 0;
+            ALUSrc <= 0;
+            ImmSrc <= 0;
             RD1 <= 0;
             RD2 <= 0;
+            PC <= 0;
+            Rd <= 0;
             ImmExt <= 0;
-            RS1 <= 0;
-            RS2 <= 0;
-            RD <= 0;
-            funct3 <= 0;
-            funct7b5 <= 0;
-            ALUSrc <= 0;
-            ALUOp <= 0;
-            Branch <= 0;
-            RegWrite <= 0;
-            MemToReg <= 0;
-        else if (enable)
-            PC <= PC_ID;
-            RD1 <= RD1_ID;
-            RD2 <= RD2_ID;
-            ImmExt <= ImmExt_ID;
-            RS1 <= RS1_ID;
-            RS2 <= RS2_ID;
-            RD <= RD_ID;
-            funct3 <= funct3_ID;
-            funct7b5 <= funct7b5_ID;
-            ALUSrc <= ALUSrc_ID;
-            ALUOp <= ALUOp_ID;
-            Branch <= Branch_ID;
-            RegWrite <= RegWrite_ID;
-            MemToReg <= MemToReg_ID;
+            PCPlus4 <= 0;
+        end else if (enable) begin
+            RegWrite <= RegWriteD;
+            ResultSrc <= ResultSrcD;
+            MemWrite <= MemWriteD;
+            Jump <= JumpD;
+            Branch <= BranchD;
+            ALUControl <= ALUControlD;
+            ALUSrc <= ALUSrcD;
+            ImmSrc <= ImmSrcD;
+            RD1 <= RD1D;
+            RD2 <= RD2D;
+            PC <= PCD;
+            Rd <= RdD;
+            ImmExt <= ImmExtD;
+            PCPlus4 <= PCPlus4D;
+        end
+            
     end
-
-    assign PC_EX = PC;
-    assign RD1_EX = RD1;
-    assign RD2_EX = RD2;
-    assign ImmExt_EX = ImmExt;
-    assign RS1_EX = RS1;
-    assign RS2_EX = RS2;
-    assign RD_EX = RD;
-    assign funct3_EX = funct3;
-    assign funct7b5_EX = funct7b5;
-    assign ALUSrc_EX = ALUSrc;
-    assign ALUOp_EX = ALUOp;
-    assign Branch_EX = Branch;
-    assign RegWrite_EX = RegWrite;
-    assign MemToReg_EX = MemToReg;
+    
+    assing RegWriteE = RegWrite;
+    assing ResultSrcE = ResultSrc;
+    assing MemWriteE = MemWrite;
+    assing JumpE = Jump;
+    assing BranchE = Branch;
+    assing ALUControlE = ALUControl;
+    assing ALUSrcE = ALUSrc;
+    assing ImmSrcE = ImmSrc;
+    assing RD1E = RD1;
+    assing RD2E = RD2;
+    assing PCE = PC;
+    assing RdE = Rd;
+    assing ImmExtE = ImmExt;
+    assing PCPlus4E = PCPlus4;
 
 endmodule
--- g12-Verilog/reg_IF_ID.v
+++ g12-Verilog/reg_IF_ID.v
@@ -2,31 +2,38 @@ module reg_IF_ID(
     input         clock,
     input         reset,
     input         enable,
-    input  [31:0] Instr_IF,
-    input  [31:0] PC_IF,
+    input  [31:0] InstrF,
+    input  [31:0] PCF,
+    input  [31:0] PCPlus4F,
 
-    output [31:0] Instr_ID,
-    output [31:0] PC_ID
+    output [31:0] InstrD,
+    output [31:0] PCD
+    output [31:0] PCPlus4D
 );
 
     reg [31:0] Instr;
     reg [31:0] PC;
+    reg [31:0] PCPlus4;
 
     initial begin
         Instr <= 0;
         PC <= 0;
+        PCPlus4 <= 0;
     end
 
     always @(posedge clock or posedge reset) begin
         if (reset)
             Instr <= 0;
             PC <= 0;
+            PCPlus4 <= 0;
         else if (enable)
-            Instr <= Instr_IF;
-            PC <= PC_IF;
+            Instr <= InstrF;
+            PC <= PCF;
+            PCPlus4 <= PCPlus4F;
     end
 
-    assign Instr_ID = Instr;
-    assign PC_ID = PC;
+    assign InstrD = Instr;
+    assign PCD = PC;
+    assign PCPlus4D = PCPlus4;
 
 endmodule
